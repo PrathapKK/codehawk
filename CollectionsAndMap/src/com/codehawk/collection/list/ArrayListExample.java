e568a21fb7f76f3f7b283422fe5a5a7b4b44bf02
prathap_kk <prathap_kk@gmail.com>
2013-08-20 10:16:32 +0530

    ArrayList of String

--- CollectionsAndMap/src/com/codehawk/collection/list/ArrayListExample.java
+++ CollectionsAndMap/src/com/codehawk/collection/list/ArrayListExample.java
@@ -10,6 +10,17 @@ public class ArrayListExample {
 		
 		ArrayList namesList = new ArrayList();
 		
+		namesList.add("Abdul");
+		namesList.add("Arun Kumar");
+		namesList.add("Arun Christopher");
+		namesList.add("Bala");
+		namesList.add(3, "Antony");
+		namesList.addAll(namesList);
+		System.out.println();
+		System.out.println("Is List Contains "+namesList.contains("Antony Doss"));
+		
+		
+		
 	}
 
 }
