d2cce7214c68e09bb39bec7f184a170c049dc9bd
prathap_kk <prathap_kk@gmail.com>
2013-08-21 19:28:31 +0530

    Date Conversion Programs

---
+++ DateConversionProjects/src/com/sample/DateConversion.java
@@ -0,0 +1,38 @@
+package com.sample;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class DateConversion {
+
+	
+	
+	 public static java.sql.Date stringToDate(String strDate) {
+
+	        String methodName = "stringToDate";
+	        java.sql.Date returnDate = new java.sql.Date(System.currentTimeMillis());
+	        try {
+	            Locale defaultLocale = Locale.getDefault();
+	            DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, defaultLocale);
+	            java.util.Date d = df.parse(strDate);
+	            returnDate = new java.sql.Date(d.getTime());
+	           
+	        } catch (ParseException pe) {
+	           System.out.println("Exception from " + methodName + " is " + pe.getMessage());
+	        }
+	        return returnDate;
+
+	    }
+	 
+	 
+	 public static void main(String args[]) {
+		 
+		 String effectiveDate = "13/14/2017";
+		 String expiryDate = "13/14/2018";
+		 System.out.println("Effective Date : "+ DateConversion.stringToDate(effectiveDate));
+		 System.out.println("Expiry Date : "+ DateConversion.stringToDate(expiryDate));
+	
+	 }
+	 
+}
